7c1c65c764a5c8d4bedd0f40cc4a16d032c4f77f
Guillermo_Sierra_Castejon <sierracastejong@gmail.com>
2025-04-25 09:43:47 +0200

    He comentado colores uqe no utilizo

--- src/utilidades/Constantes.java
+++ src/utilidades/Constantes.java
@@ -11,27 +11,27 @@ import java.time.format.DateTimeFormatter;
 public class Constantes {
     public static final String COLOR_RESET = "\u001B[0m";
 
-    public static final String COLOR_BLACK = "\u001B[30m";
+    // public static final String COLOR_BLACK = "\u001B[30m";
     public static final String COLOR_RED = "\u001B[31m";
     public static final String COLOR_GREEN = "\u001B[32m";
     public static final String COLOR_YELLOW = "\u001B[33m";
     public static final String COLOR_BLUE = "\u001B[34m";
     public static final String COLOR_PURPLE = "\u001B[35m";
     public static final String COLOR_CYAN = "\u001B[36m";
-    public static final String COLOR_WHITE = "\u001B[37m";
+    // public static final String COLOR_WHITE = "\u001B[37m";
 
-    public static final String BG_BLACK = "\u001B[40m";
-    public static final String BG_RED = "\u001B[41m";
-    public static final String BG_GREEN = "\u001B[42m";
-    public static final String BG_YELLOW = "\u001B[43m";
-    public static final String BG_BLUE = "\u001B[44m";
-    public static final String BG_PURPLE = "\u001B[45m";
-    public static final String BG_CYAN = "\u001B[46m";
-    public static final String BG_WHITE = "\u001B[47m";
+    // public static final String BG_BLACK = "\u001B[40m";
+    // public static final String BG_RED = "\u001B[41m";
+    // public static final String BG_GREEN = "\u001B[42m";
+    // public static final String BG_YELLOW = "\u001B[43m";
+    // public static final String BG_BLUE = "\u001B[44m";
+    // public static final String BG_PURPLE = "\u001B[45m";
+    // public static final String BG_CYAN = "\u001B[46m";
+    // public static final String BG_WHITE = "\u001B[47m";
 
-    public static final String STYLE_BOLD = "\u001B[1m";
-    public static final String STYLE_UNDERLINED = "\u001B[4m";
-    public static final String STYLE_REVERSED = "\u001B[7m";
+    // public static final String STYLE_BOLD = "\u001B[1m";
+    // public static final String STYLE_UNDERLINED = "\u001B[4m";
+    // public static final String STYLE_REVERSED = "\u001B[7m";
 
     public static final int FINALIZARPROGRAMA = 6;
 
